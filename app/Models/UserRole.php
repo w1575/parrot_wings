fa66cca7657e5681743bdf20503f01a42f12012e
w <w1575@ya.ru>
2021-06-24 01:02:47 +0500

    UserRole.php some settings

--- app/Models/UserRole.php
+++ app/Models/UserRole.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class UserRole
+ * @package App\Models
+ * @mixin Builder
+ */
 class UserRole extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+
+    public const ROLE_ADMIN = 1;
+    public const ROLE_MEMBER = 0;
+
+    /**
+     * @var string[]
+     */
+    public $fillable = [
+        'user_id',
+        'role'
+    ];
+
 }
